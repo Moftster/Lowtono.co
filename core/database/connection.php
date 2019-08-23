6edbe1274afdb328a538e8ad1e6a29ac2b46a72f
David Moftakhar <david@sellmysnaps.com>
2019-08-23 18:55:03 +0100

    Updated server details for production

--- core/database/connection.php
+++ core/database/connection.php
@@ -5,14 +5,14 @@
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 // Local host code
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
+// $dbServername = "localhost";
+// $dbUsername = "root";
+// $dbPassword = "";
 
 // Production code
-// $dbServername = $url["host"];
-// $dbUsername = $url["user"];
-// $dbPassword = $url["pass"];
+$dbServername = $url["host"];
+$dbUsername = $url["user"];
+$dbPassword = $url["pass"];
 $dbName = "heroku_75a209a499a855d";
 
 // $dbName = substr($url["path"], 1);
