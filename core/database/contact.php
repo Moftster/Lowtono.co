69ee8d46aa7c96262cfadb2731016fb6076bd4b4
David Moftakhar <david@sellmysnaps.com>
2019-08-27 19:06:08 +0100

    Updated contact script in line with SendGried devcenter documentation.

--- core/database/contact.php
+++ core/database/contact.php
@@ -4,27 +4,44 @@ if(isset($_POST['submit'])){
 
     // echo "Connected to file";
 
-    $to = "d_moftakhar@hotmail.com"; // this is your Email address
+    // $to = "d_moftakhar@hotmail.com"; // this is your Email address
+    //
+    // $from = $_POST['email']; // this is the sender's Email address
+    //
+    // $name = $_POST['name'];
+    //
+    // $subject = "Lowtono.co contact form submission";
+    //
+    // $subject2 = "Copy of your form submission";
+    //
+    // $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+    //
+    // $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+    //
+    // $headers = "From:" . $from;
+    //
+    // $headers2 = "From:" . $to;
+    //
+    // mail($to,$subject,$message,$headers);
+    //
+    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
 
-    $from = $_POST['email']; // this is the sender's Email address
+    require 'vendor/autoload.php';
 
-    $name = $_POST['name'];
 
-    $subject = "Lowtono.co contact form submission";
+    $from = new SendGrid\Email(null, "david@sellmysnaps.com");
+    $subject = "Hello World from the SendGrid PHP Library!";
+    $to = new SendGrid\Email(null, "david@sellmysnaps.com");
+    $content = new SendGrid\Content("text/plain", "Hello, Email!");
+    $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-    $subject2 = "Copy of your form submission";
+    $apiKey = getenv('SENDGRID_API_KEY');
+    $sg = new \SendGrid($apiKey);
 
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
-
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-
-    $headers2 = "From:" . $to;
-
-    mail($to,$subject,$message,$headers);
-
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    $response = $sg->client->mail()->send()->post($mail);
+    echo $response->statusCode();
+    echo $response->headers();
+    echo $response->body();
 
     $message = "Thank you " . $name . ", we will contact you shortly.";
 
