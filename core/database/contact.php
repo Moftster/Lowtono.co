baed42a067a39cec514d5e7d3fafa4b2b5ba79bf
David Moftakhar <david@sellmysnaps.com>
2019-08-29 08:10:01 +0100

    Previous contact.php script

--- core/database/contact.php
+++ core/database/contact.php
@@ -4,44 +4,44 @@ if(isset($_POST['submit'])){
 
     // echo "Connected to file";
 
-    // $to = "d_moftakhar@hotmail.com"; // this is your Email address
-    //
-    // $from = $_POST['email']; // this is the sender's Email address
-    //
-    // $name = $_POST['name'];
-    //
-    // $subject = "Lowtono.co contact form submission";
-    //
-    // $subject2 = "Copy of your form submission";
-    //
-    // $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
-    //
-    // $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
-    //
-    // $headers = "From:" . $from;
-    //
-    // $headers2 = "From:" . $to;
-    //
-    // mail($to,$subject,$message,$headers);
-    //
-    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    $to = "d_moftakhar@hotmail.com"; // this is your Email address
 
-    require 'vendor/autoload.php';
+    $from = $_POST['email']; // this is the sender's Email address
 
+    $name = $_POST['name'];
 
-    $from = new SendGrid\Email(null, "david@sellmysnaps.com");
-    $subject = "Hello World from the SendGrid PHP Library!";
-    $to = new SendGrid\Email(null, "david@sellmysnaps.com");
-    $content = new SendGrid\Content("text/plain", "Hello, Email!");
-    $mail = new SendGrid\Mail($from, $subject, $to, $content);
+    $subject = "Lowtono.co contact form submission";
 
-    $apiKey = getenv('SENDGRID_API_KEY');
-    $sg = new \SendGrid($apiKey);
+    $subject2 = "Copy of your form submission";
 
-    $response = $sg->client->mail()->send()->post($mail);
-    echo $response->statusCode();
-    echo $response->headers();
-    echo $response->body();
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+
+    $headers2 = "From:" . $to;
+
+    mail($to,$subject,$message,$headers);
+
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+
+    // require __DIR__ . '/../../vendor/autoload.php';
+    //
+    //
+    // $from = new SendGrid\Email(null, "david@sellmysnaps.com");
+    // $subject = "Hello World from the SendGrid PHP Library!";
+    // $to = new SendGrid\Email(null, "david@sellmysnaps.com");
+    // $content = new SendGrid\Content("text/plain", "Hello, Email!");
+    // $mail = new SendGrid\Mail($from, $subject, $to, $content);
+    //
+    // $apiKey = getenv('SENDGRID_API_KEY');
+    // $sg = new \SendGrid($apiKey);
+    //
+    // $response = $sg->client->mail()->send()->post($mail);
+    // echo $response->statusCode();
+    // echo $response->headers();
+    // echo $response->body();
 
     $message = "Thank you " . $name . ", we will contact you shortly.";
 
